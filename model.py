d4cf9cf2a22eae9b86ca492ef66c9f7fd9c681e6
Syafuuuuu <syafuedu@gmail.com>
2024-07-21 18:22:39 +0800

    Yeet

---
+++ model.py
@@ -0,0 +1,17 @@
+#Emotion setting and array
+Hp = 0.7
+Sd = 0.2
+Fr = 0.2
+An = 0.1
+Em = [Hp, Sd, Fr, An]
+
+Nu = 0.2
+Ex = 0.9
+Op = 0.6
+Ps = [Nu, Ex, Op]
+
+Ap = 0.0 #Positive Affect
+Iw = 0.0 #Interaction Willingness
+
+Ap = ()
+
